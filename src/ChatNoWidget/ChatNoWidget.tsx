8b872b336d15c30ccfb3f0a6b1a33e6faa45f338
Nishant Nagururu <nishantnvn05@gmail.com>
2024-08-13 11:35:44 -0400

    whatever

--- src/ChatNoWidget/ChatNoWidget.tsx
+++ src/ChatNoWidget/ChatNoWidget.tsx
@@ -143,8 +143,8 @@ function ChatNoWidget({closeChat}: ChatNoWidgetProps) {
   const getCurrentPage = (): string | null => {
     const currentPage =  localStorage.getItem('currentPage');
     if (!currentPage) {
-      localStorage.setItem('currentPage', 'main-menu');
-      return 'main-menu';
+      localStorage.setItem('currentPage', 'user-select');
+      return 'user-select';
     }
     return currentPage;
   }
@@ -160,6 +160,10 @@ function ChatNoWidget({closeChat}: ChatNoWidgetProps) {
     changeCurrentPage('chat')
   }
 
+  const userSelectToMainMenu = (user: string) => {
+    setUserType(user);
+    changeCurrentPage('main-menu')
+  }
   const getSessionId = (): string | null => {
     return localStorage.getItem('candorSessionID');
   };
@@ -304,21 +308,58 @@ function ChatNoWidget({closeChat}: ChatNoWidgetProps) {
 
   const isFormFilled = firstName && lastName && email && phoneNumber;
 
+  const renderUserTypeSelect = () => (
+    <div id="mainMenu">
+      {userType && (
+        <>
+          <button className="menuButton" onClick={() => {userSelectToMainMenu("prospectiveResident")}}>
+            Looking To Rent A Home?
+            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#FFFFFF">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path d="M8 10.5H16" stroke="#ffffff" stroke-width="2.5" stroke-linecap="round"></path>
+                <path d="M8 14H13.5" stroke="#ffffff" stroke-width="2.5" stroke-linecap="round"></path>
+                <path d="M17 3.33782C15.5291 2.48697 13.8214 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22C17.5228 22 22 17.5228 22 12C22 10.1786 21.513 8.47087 20.6622 7" stroke="#ffffff" stroke-width="2.5" stroke-linecap="round"></path>
+              </g>
+            </svg>
+          </button>
+          <button className="menuButton" onClick={() => {userSelectToMainMenu("currentResident")}}>
+            Current Tenant
+            <svg width="25px" height="25px" viewBox="0 -2.5 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#ffffff">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <title>email [#ffffff]</title>
+                <desc>Created with Sketch.</desc>
+                <defs></defs>
+                <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                  <g id="Dribbble-Light-Preview" transform="translate(-300.000000, -922.000000)" fill="#ffffff">
+                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                      <path d="M262,764.291 L254,771.318 L246,764.281 L246,764 L262,764 L262,764.291 Z M246,775 L246,766.945 L254,773.98 L262,766.953 L262,775 L246,775 Z M244,777 L264,777 L264,762 L244,762 L244,777 Z" id="email-[#ffffff]"></path>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </svg>
+          </button>
+          <button className="menuButton" onClick={() => {userSelectToMainMenu("owner")}}>
+            Property Owner
+            <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier">
+                <path d="M3 9H21M7 3V5M17 3V5M6 12H8M11 12H13M16 12H18M6 15H8M11 15H13M16 15H18M6 18H8M11 18H13M16 18H18M6.2 21H17.8C18.9201 21 19.4802 21 19.908 20.782C20.2843 20.5903 20.5903 20.2843 20.782 19.908C21 19.4802 21 18.9201 21 17.8V8.2C21 7.07989 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V17.8C3 18.9201 3 19.4802 3.21799 19.908C3.40973 20.2843 3.71569 20.5903 4.09202 20.782C4.51984 21 5.07989 21 6.2 21Z" stroke="#ffffff" stroke-width="2" stroke-linecap="round"></path>
+              </g>
+            </svg>
+          </button>
+        </>
+      )}
+    </div>
+  )
+
   const renderMainMenu = () => (
     <div id="mainMenu">
-      <div className="dropdownContainer">
-        <label htmlFor="userTypeDropdown">Select what best describes you:</label>
-        <select
-          id="userTypeDropdown"
-          value={userType || ""}
-          onChange={(e) => handleUserTypeChange(e.target.value)}
-        >
-          <option value="">Select an option</option>
-          <option value="prospectiveResident">Looking to Rent a Home</option>
-          <option value="currentResident">Current Tenant</option>
-          <option value="owner">Property Owner</option>
-        </select>
-      </div>
       {userType && (
         <>
           <button className="menuButton" onClick={menuToChatPage}>
@@ -364,7 +405,6 @@ function ChatNoWidget({closeChat}: ChatNoWidgetProps) {
           </button>
         </>
       )}
-      
     </div>
   );
 
@@ -506,7 +546,7 @@ function ChatNoWidget({closeChat}: ChatNoWidgetProps) {
           <button id="closeButton" onClick={closeChat}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" className="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg></button>
         )}
       </div>
-      {currentPage === 'main-menu' ? renderMainMenu() : renderChatInterface()}
+      {currentPage === 'main-menu' ? renderMainMenu() : currentPage === 'user-select' ? renderUserTypeSelect() : renderChatInterface()}
       {showContactForm && (
         <div id="contactFormContainer">
           <div id="contactForm">
