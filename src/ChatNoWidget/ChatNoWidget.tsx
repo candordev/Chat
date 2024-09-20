c1083bfb84f056eed47cb00f5585f2814029f272
Nishant Nagururu <nishantnvn05@gmail.com>
2024-09-20 13:18:49 -0400

    change config if statement

--- src/ChatNoWidget/ChatNoWidget.tsx
+++ src/ChatNoWidget/ChatNoWidget.tsx
@@ -71,11 +71,9 @@ function ChatNoWidget({closeChat, mobile}: ChatNoWidgetProps) {
     // Set groupId based on the environment
     console.log('useEffect called');
     const initialize = async () => {
-      if (config.environment === 'localhost') {
-        setGroupId('6657a0e9d9f0ae27bd3e0021'); // DEV groupId
-      } else if (config.environment === 'candoradmin') {
+      if (config.environment === 'candoradmin') {
         setGroupId('663fa89af38d72f0490da655'); // PRODUCTION groupId
-      } else if (config.environment === 'productionTest') {
+      } else {
         setGroupId('6657a0e9d9f0ae27bd3e0021'); // DEV groupId
       }
 
